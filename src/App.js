2f641e2018c907eb596774419ec993e5911ddc9b
Greg Rakozy <greg@solvvy.com>
2019-03-18 19:15:10 -0600

    first commit of mvp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+// import moment from "moment";
+
+const API =
+  "https://sheets.googleapis.com/v4/spreadsheets/19V3ktxaj4YnrO3C3ocp4-jj1QTJI1ZCEG7aRl2Jlt1Q/values:batchGet?ranges=user_entries&majorDimension=ROWS&key=AIzaSyC6GyiXpiMeYlcf8vhLygKe3FHQuOGoQUY";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      conditions: []
+    };
+  }
+  componentDidMount() {
+    fetch(API)
+      .then(response => response.json())
+      .then(data => {
+        let batchRowValues = data.valueRanges[0].values;
+
+        let rows = [];
+        for (let i = 1; i < batchRowValues.length; i++) {
+          let rowObject = {};
+          for (let j = 0; j < batchRowValues[i].length; j++) {
+            rowObject[batchRowValues[0][j]] = batchRowValues[i][j];
+          }
+          rows.push(rowObject);
+        }
+        this.setState({ conditions: rows.reverse() });
+        console.log(this.state.conditions);
+      });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+      <div>
+        <h1>Recent Reports</h1>
+        <div className="button">
           <a
-            className="App-link"
-            href="https://reactjs.org"
+            className="myButton"
             target="_blank"
             rel="noopener noreferrer"
+            href="https://goo.gl/forms/qKQ92PIjBfslJL0y1"
           >
-            Learn React
+            Add New Report
           </a>
-        </header>
+        </div>
+        <div className="reports">
+          <div className="report">
+            {this.state.conditions.map(report => (
+              <div
+                key={report.id}
+                className="report-details"
+                id={report.status}
+              >
+                <div className="trail">
+                  <p className="label">Trail</p>
+                  {report.trail}
+                </div>
+                <div className="status">
+                  <p className="label">{report.timestamp}</p>
+                  {report.status}
+                </div>
+                <div className="details">
+                  <p className="label">Details</p>
+                  {report.conditions}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  api:
+    "https://sheets.googleapis.com/v4/spreadsheets/19V3ktxaj4YnrO3C3ocp4-jj1QTJI1ZCEG7aRl2Jlt1Q/values:batchGet?ranges=user_entries&majorDimension=ROWS&key=AIzaSyC6GyiXpiMeYlcf8vhLygKe3FHQuOGoQUY"
+};
